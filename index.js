061414a65ef53cc1d37183368e0b7848e7a1018e
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-11 22:13:05 +0530

    feat : fs module

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Aryan started Backend");
+const http = require("http");
+const fs = require("fs");
+
+console.log(fs.readFileSync("./sample.txt", "utf-8"));
+
+// const ans = http.createServer((req, res, next) => {
+// 	req();
+// });
